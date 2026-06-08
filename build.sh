fb4e9126e8ce09b2766aaf66bd7c2f502b6eeb07
rintfosk <nyszeng@gmail.com>
2026-06-08 12:22:46 +0800

    Update build script to create nested directory for weapon history and adjust copy command

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 
-mkdir -p dist
+mkdir -p dist/data/weapon-history
 cp index.html styles.css viewer.js dist/
-cp -r data/weapon-history dist/data/weapon-history
+cp -r data/weapon-history/. dist/data/weapon-history/
